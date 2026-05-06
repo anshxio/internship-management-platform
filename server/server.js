2a05fc455f0100e18befb413f14cfba73413efc9
Ansh Sharma <rishu11sharm@gmail.com>
2026-05-07 04:19:37 +0530

    Intitial backend authentication setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pool = require("./db/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/", async (req, res) =>{
+    try{
+        const result = await pool.query("SELECT NOW()");
+        res.send(result.rows[0]);
+    }catch(err){
+        console.error(err);
+        res.send("Database connection error");
+    }
+})
+
+app.listen(5000, () =>{
+    console.log("Server is running on port 5000");
+});
