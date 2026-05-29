d9200199ce0d52fa9149abe4571dfc4e29026cd5
Ansh Sharma <rishu11sharm@gmail.com>
2026-05-29 23:00:23 +0530

    Implemented internship posting and dynamic listing

--- client/src/components/FeatureInternship.jsx
+++ client/src/components/FeatureInternship.jsx
@@ -1,6 +1,22 @@
+import { useEffect,useState} from "react";
+import axios from "axios";
 import InternshipCard from "./InternshipCard";
 
 function FeatureInternship() {
+  const [internships,setInternships] = useState([]);
+  useEffect(()=>{
+    const fetchInternships = async () => {
+      try{
+        const res = await axios.get("http://localhost:5000/api/internships");
+        setInternships(res.data.internships);
+      } catch(err){
+        console.error(err);
+
+      }
+    };
+    fetchInternships();
+  },[]);
+  console.log(internships);
   return (
     <section className="px-10 py-24">
       {/* HEADER */}
@@ -17,62 +33,19 @@ function FeatureInternship() {
 
       {/* GRID */}
       <div className="grid grid-cols-3 gap-10 mt-20">
-        <InternshipCard
+
+        {internships.map((internship) =>(
+          <InternshipCard
           icon="🚀"
-          title="Frontend Developer Intern"
-          company="TechCorp"
-          location="Remote"
-          stipend="25,000/mo"
-          posted="2 days ago"
+          key ={internship.id}
+          title={internship.title}
+          company= {internship.company}
+          location={internship.location}
+          stipend={internship.stipend}
+          posted= {new Date(internship.created_at).toLocaleDateString()}
           skills={["React", "TypeScript", "Tailwind"]}
         />
-
-        <InternshipCard
-          icon="📊"
-          title="Data Science Intern"
-          company="DataFlow"
-          location="Remote"
-          stipend="30,000/mo"
-          posted="1 week ago"
-          skills={["Python", "ML", "Pandas"]}
-        />
-
-        <InternshipCard
-          icon="🎨"
-          title="UI/UX Design Intern"
-          company="DesignHub"
-          location="Delhi"
-          stipend="20,000/mo"
-          posted="3 days ago"
-          skills={["Figma", "Design", "Prototyping"]}
-        />
-        <InternshipCard
-          icon="☁️"
-          title="DevOps Intern"
-          company="CloudSys"
-          location="Bangalore"
-          stipend="22,000/mo"
-          posted="5 days ago"
-          skills={["AWS", "Docker", "CI/CD"]}
-        />
-        <InternshipCard
-          icon="📱"
-          title="Mobile Dev Intern"
-          company="MobileTech"
-          location="Bangalore"
-          stipend="25,000/mo"
-          posted="1 day ago"
-          skills={["React Native", "iOS", "Android"]}
-        />
-        <InternshipCard
-          icon="🤖"
-          title="AI Research Intern"
-          company="AILabs"
-          location="Gurgaon"
-          stipend="30,000/mo"
-          posted="4 days ago"
-          skills={["PyTorch", "Research", "NLP"]}
-        />
+        ) )}
       </div>
       <div className="flex justify-center mt-16">
         <button className="bg-[#08122F] text-white px-10 py-4 rounded-2xl border-2 border-gray-300 text-xl font-semibold hover:bg-[#0B173A] transition-all duration-300 hover:scale-105">
