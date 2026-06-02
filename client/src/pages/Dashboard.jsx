bb5592c4eb109ce7f134a292d142d90ec69bd82e
Ansh Sharma <rishu11sharm@gmail.com>
2026-06-02 16:20:56 +0530

    Change the Sidebar ---> SideBar causing error

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import SideBar from "../components/Sidebar";
+import SideBar from "../components/SideBar";
 import DashboardStatCard from "../components/DashboardStatCard";
 import RecommendationCard from "../components/RecommendationCard";
 
